2a35d1c81425cee2e291df712ab83e615bbbbc44
haya14busa <haya14busa@gmail.com>
2019-09-15 08:04:29 +0000

    Prefer to use INPUT_{env} for docker action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,7 @@
 
 cd "$GITHUB_WORKSPACE"
 
-export REVIEWDOG_GITHUB_API_TOKEN="$1"
-GOLANGCI_LINT_FLAGS="$2"
-TOOL_NAME="$3"
-LEVEL="$4"
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ -z "${REVIEWDOG_GITHUB_API_TOKEN}" ]; then
-  if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
-    echo "INPUT_GITHUB_TOKEN not found"
-    exit 1
-  else
-    export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
-  fi
-fi
-
-golangci-lint run --out-format line-number ${GOLANGCI_LINT_FLAGS} \
-  | reviewdog -f=golangci-lint -name="${TOOL_NAME}" -reporter=github-pr-check -level="${LEVEL}"
+golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
+  | reviewdog -f=golangci-lint -name="${INPUT_TOOL_NAME}" -reporter=github-pr-check -level="${INPUT_LEVEL}"
