7cf1066e7551a5cdbe910de74dde0b40961a62f8
haya14busa <haya14busa@gmail.com>
2020-05-16 08:14:04 +0000

    debug dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ cd "${GITHUB_WORKSPACE}/${INPUT_DIRECTORY}" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+pwd
+ls -a -l
+
 # shellcheck disable=SC2086
 golangci-lint run --out-format line-number ${INPUT_GOLANGCI_LINT_FLAGS} \
   | reviewdog -f=golangci-lint \
