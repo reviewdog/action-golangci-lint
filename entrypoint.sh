87b19204c0e895876ea2d7975da93b054e977d82
haya14busa <haya14busa@gmail.com>
2020-08-15 08:42:50 +0000

    Migrate from docker to composite run actions
    https://docs.github.com/en/actions/creating-actions/creating-a-composite-run-steps-action
    
    It should be able to reuse the go build cache and share the same build
    context/environment by using the same default workspace instead of using Docker.
    
    It should resolve an issue such as #21 as well.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,17 @@
 
 cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit 1
 
+TEMP_PATH="$(mktemp -d)"
+PATH="${TEMP_PATH}:$PATH"
+
+echo '::group::🐶 Installing reviewdog ... https://github.com/reviewdog/reviewdog'
+curl -sfL https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${REVIEWDOG_VERSION}" 2>&1
+echo '::endgroup::'
+
+echo '::group::🐶 Installing golangci-lint ... https://github.com/golangci/golangci-lint'
+curl -sfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "${TEMP_PATH}" "${GOLANGCI_LINT_VERSION}" 2>&1
+echo '::endgroup::'
+
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 # shellcheck disable=SC2086
