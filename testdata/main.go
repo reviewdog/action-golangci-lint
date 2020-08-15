ae8a740eb0b6d6d8071340e78e9eb607e908f132
haya14busa <haya14busa@gmail.com>
2020-08-15 08:57:21 +0000

    debug: update test code data for debug

--- testdata/main.go
+++ testdata/main.go
@@ -1,21 +1,21 @@
 package testdata
 
 import (
-	"os"
 	"fmt"
+	"os"
 )
 
-func Main(    ) {
-	// langauge
+func Main() {
+	//  langauge
 
-	x    := 2
+	x := 2
 	x += 1
 	fmt.Sprintf("%d")
 
-	os.Open("main.go")
+	os.Open("main1.go")
 }
 
-func unused(unusedParam int) error {
+func unused1(unusedParam int) error {
 
 	return nil
 
